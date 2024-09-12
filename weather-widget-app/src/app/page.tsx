e1e712d87c47ef59af0f802728f12732b4aec318
Alishba-Moin <alishbamoin797@gmail.com>
2024-09-12 20:19:37 +0500

    weather-widget-app

--- weather-widget-app/src/app/page.tsx
+++ weather-widget-app/src/app/page.tsx
@@ -1,13 +1,9 @@
-import WeatherWidget from "@/components/ui/weather-widget";
+import WeatherWidget from "@/components/weather-widget";
 
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
       <WeatherWidget/>
-=======
-      <WeatherWidget />
->>>>>>> 6815f5945ffc3ab3ca774d5ba916ac80931a1c24
-    </div>
+      </div>
   );
 }
