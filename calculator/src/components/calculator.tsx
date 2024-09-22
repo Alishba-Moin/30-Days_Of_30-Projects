09e5d09b4d5936d4974d47473a7ba967cd167cc4
Alishba Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-09-22 05:12:50 +0500

    calculator-project

--- calculator/src/components/calculator.tsx
+++ calculator/src/components/calculator.tsx
@@ -1,4 +1,4 @@
-"use client"; // Enables client-side rendering 
+"use client"; // Enables Client-Side rendering 
 
 import { useState, ChangeEvent, useEffect } from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
